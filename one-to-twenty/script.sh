88ecfd50d44f05a6b82a1627ac6b2f73ada2201d
Muhammad Haseeb <haseeb241202@gmail.com>
2025-05-16 14:28:17 +0500

    complete 08 question

--- one-to-twenty/script.sh
+++ one-to-twenty/script.sh
@@ -98,3 +98,10 @@ fi
 if [[ "$a" != "$b" && "$b" != "$c" && "$a" != "$c" ]]; then
     echo "SCALENE"
 fi
+
+
+# question 09
+# Given N lines of input, print the 3rd  character from each line as a new line of output. It is guaranteed that each of the n lines of input will have a 3rd character.
+
+
+cut -c 3
