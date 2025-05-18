d6f2d8d1aa0b4f310f03452c16403320233f7001
haseeb2412 <haseeb241202@gmail.com>
2025-05-19 02:16:14 +0500

    complete question cut-8

--- one-to-twenty/script.sh
+++ one-to-twenty/script.sh
@@ -126,3 +126,8 @@ cut -c 13-
 # Given a sentence, identify and display its fourth word. Assume that the space (' ') is the only delimiter between
 
 cut -d '' -f 4
+
+# question 14
+# Given a sentence, identify and display its first three words. Assume that the space (' ') is the only delimiter between words.
+
+cut -d ' ' -f 0-3
