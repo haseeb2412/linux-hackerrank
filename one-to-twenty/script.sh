b6afb9ffa1e18fb3b482269a5188afdfdb7d5d46
haseeb2412 <haseeb241202@gmail.com>
2025-05-20 01:19:00 +0500

    complete question ( text processing tail 02)

--- one-to-twenty/script.sh
+++ one-to-twenty/script.sh
@@ -160,3 +160,9 @@ sed -n "12,22p"
 # Display the last  lines of an input file.
 
 tail -n 20
+
+# question ( text processing tail-02 )
+# In this challenge, we practice using the tail command to display the last  characters of a text file.
+# Display the last  characters of an input file.
+
+tail -c 20
